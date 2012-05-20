e6bc21485c70766c549ae505b0600ddfe05df78a
Pavel Brylov <pavel@brylov.info>
2012-05-20 21:26:49 +0300

    Fallback for empty translations => true

--- lib/spree_product_translations.rb
+++ lib/spree_product_translations.rb
@@ -11,35 +11,35 @@ module SpreeProductTranslations
       end
 
       Spree::Product.class_eval do
-        translates :name, :description, :meta_description, :meta_keywords, :copy => true
+        translates :name, :description, :meta_description, :meta_keywords, :copy => true, :fallbacks_for_empty_translations => true
       end
 
       Spree::ProductProperty.class_eval do
-        translates :value, :copy => true
+        translates :value, :copy => true, :fallbacks_for_empty_translations => true
       end
 
       Spree::Property.class_eval do
-        translates :presentation
+        translates :presentation, :fallbacks_for_empty_translations => true
       end
 
       Spree::Prototype.class_eval do
-        translates :name, :copy => true
+        translates :name, :copy => true, :fallbacks_for_empty_translations => true
       end
 
       Spree::Taxonomy.class_eval do
-        translates :name, :copy => true
+        translates :name, :copy => true, :fallbacks_for_empty_translations => true
       end
 
       Spree::Taxon.class_eval do
-        translates :name, :description, :copy => true
+        translates :name, :description, :copy => true, :fallbacks_for_empty_translations => true
       end
 
       Spree::OptionType.class_eval do
-        translates :presentation
+        translates :presentation, :fallbacks_for_empty_translations => true
       end
 
       Spree::OptionValue.class_eval do
-        translates :presentation
+        translates :presentation, :fallbacks_for_empty_translations => true
       end
 
       # Enable I18n fallbacks
