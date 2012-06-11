e948771508d52cf872097dfffa3ba99b1d90767f
Pavel Brylov <pavel@brylov.info>
2012-06-12 00:08:59 +0300

    accept_nested_attributes for translations

--- lib/spree_product_translations.rb
+++ lib/spree_product_translations.rb
@@ -12,34 +12,50 @@ module SpreeProductTranslations
 
       Spree::Product.class_eval do
         translates :name, :description, :meta_description, :meta_keywords, :copy => true, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::ProductProperty.class_eval do
         translates :value, :copy => true, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::Property.class_eval do
         translates :presentation, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::Prototype.class_eval do
         translates :name, :copy => true, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::Taxonomy.class_eval do
         translates :name, :copy => true, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::Taxon.class_eval do
         translates :name, :description, :copy => true, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::OptionType.class_eval do
         translates :presentation, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       Spree::OptionValue.class_eval do
         translates :presentation, :fallbacks_for_empty_translations => true
+        attr_accessible :translations_attributes
+        accepts_nested_attributes_for :translations
       end
 
       # Enable I18n fallbacks
